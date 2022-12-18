7ba5d760e649cc4b5c0fa308cdb6c3b31462661d
Emerzon Javier Kolki Martinez <emerzonkolki@gmail.com>
2022-12-17 21:42:47 -0500

    feat(pages): complete signIn and signUp pages

--- src/app/pages/signin/signin.component.ts
+++ src/app/pages/signin/signin.component.ts
@@ -1,7 +1,21 @@
 import { Component } from '@angular/core'
+import { UserSignIn } from '../../models/User.model'
+
+import { UserService } from '../../services/user/user.service'
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
 })
-export class SigninComponent {}
+export class SigninComponent {
+  userSignIn: UserSignIn = {
+    email: '',
+    password: '',
+  }
+
+  constructor(private userService: UserService) {}
+
+  onSubmit(): void {
+    this.userService.onSignIn(this.userSignIn)
+  }
+}
--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,7 +1,24 @@
 import { Component } from '@angular/core'
+import { UserSignUp } from 'src/app/models/User.model'
+
+import { UserService } from 'src/app/services/user/user.service'
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
 })
-export class SignupComponent {}
+export class SignupComponent {
+  userSignUp: UserSignUp = {
+    first_name: '',
+    last_name: '',
+    username: '',
+    email: '',
+    password: '',
+  }
+
+  constructor(private userService: UserService) {}
+
+  onSubmit(): void {
+    this.userService.onSignUp(this.userSignUp)
+  }
+}
