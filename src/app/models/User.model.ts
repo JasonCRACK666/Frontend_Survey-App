20f67613e21d18aee3ba50568c31557cdbe93010
Emerzon Javier Kolki Martinez <emerzonkolki@gmail.com>
2022-12-17 21:41:29 -0500

    feat(models): add User and Survey model

---
+++ src/app/models/User.model.ts
@@ -0,0 +1,22 @@
+export interface User {
+  id: string
+  first_name: string
+  last_name: string
+  username: string
+  email: string
+  password: string
+  is_admin: boolean
+}
+
+export interface UserSignIn {
+  email: string
+  password: string
+}
+
+export interface UserSignUp {
+  first_name: string
+  last_name: string
+  username: string
+  email: string
+  password: string
+}
